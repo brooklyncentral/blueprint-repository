8ef03f7256cc8d61f7ec15f658d0cfc2d7931295
graeme.miller <graeme.miller@cloudsoftcorp.com>
2016-12-14 16:45:44 +0000

    Revert "removed everything but clocker to speed up development"
    This reverts commit 0fa8a1d9ecc6918014fecf4c812178ae33309dad.

--- validation-testing/src/test/java/org/brooklyncentral/catalog/validation/testing/PullRequestValidationTest.java
+++ validation-testing/src/test/java/org/brooklyncentral/catalog/validation/testing/PullRequestValidationTest.java
@@ -1,6 +1,7 @@
 package org.brooklyncentral.catalog.validation.testing;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.Collections.singleton;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
@@ -8,6 +9,7 @@ import io.cloudsoft.catalog.util.CatalogValidator;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.StringReader;
 import java.util.Arrays;
@@ -16,8 +18,10 @@ import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.lib.ObjectId;
@@ -37,6 +41,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.io.Files;
 
